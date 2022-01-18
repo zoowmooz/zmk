74fe75a39523e2a04a341280587f3baeb69dfeb7
KyouTansan <gm0t5166666@gmail.com>
2022-01-18 21:23:54 +0900

    fix clang-format

--- app/src/trackpoint.c
+++ app/src/trackpoint.c
@@ -32,7 +32,7 @@ const struct device *trackpoint;
 static int zmk_trackpoint_update(const struct device *trackpoint) {
     struct sensor_value state_of_charge;
     // LOG_DBG("zmk_trackpoint_update:name=%s",trackpoint->name);
-    
+
     int rc = sensor_sample_fetch_chan(trackpoint, SENSOR_CHAN_ACCEL_XYZ);
     if (rc != 0) {
         LOG_DBG("Failed to fetch trackpoint values: %d", rc);
@@ -46,15 +46,13 @@ static int zmk_trackpoint_update(const struct device *trackpoint) {
     }
 
     // LOG_DBG("X:%5d Y:%5d before by 10 times", state_of_charge.val1, state_of_charge.val2);
-    if (state_of_charge.val1 != 0 || state_of_charge.val2 !=0){
-      	struct zmk_hid_mouse_report *mouse_report = zmk_hid_get_mouse_report();
-	mouse_report->body.y = state_of_charge.val1 > 0 ?
-	  (int16_t)(state_of_charge.val1/12) * -1:
-	  (int16_t)(state_of_charge.val1/30) * -1;
-	mouse_report->body.x = state_of_charge.val2 > 0 ?
-	  (int16_t)(state_of_charge.val2/20):
-	  (int16_t)(state_of_charge.val2/25);
-	zmk_endpoints_send_mouse_report();
+    if (state_of_charge.val1 != 0 || state_of_charge.val2 != 0) {
+        struct zmk_hid_mouse_report *mouse_report = zmk_hid_get_mouse_report();
+        mouse_report->body.y = state_of_charge.val1 > 0 ? (int16_t)(state_of_charge.val1 / 12) * -1
+                                                        : (int16_t)(state_of_charge.val1 / 30) * -1;
+        mouse_report->body.x = state_of_charge.val2 > 0 ? (int16_t)(state_of_charge.val2 / 20)
+                                                        : (int16_t)(state_of_charge.val2 / 25);
+        zmk_endpoints_send_mouse_report();
     }
     return rc;
 }
@@ -76,11 +74,11 @@ K_TIMER_DEFINE(trackpoint_timer, zmk_trackpoint_timer, NULL);
 static int zmk_trackpoint_init(const struct device *_arg) {
     trackpoint = device_get_binding("TRACKPOINT");
     if (trackpoint == NULL) {
-      	LOG_DBG("No trackpoint device labelled TRACKPOINT found.");
-	return -ENODEV;
+        LOG_DBG("No trackpoint device labelled TRACKPOINT found.");
+        return -ENODEV;
     }
     zmk_hid_mouse_clear();
-    
+
     int rc = zmk_trackpoint_update(trackpoint);
 
     if (rc != 0) {
