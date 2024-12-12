496933e79c596d2f0ba5617aef385c5aa7683d32
KyouTansan <gm0t5166666@gmail.com>
2024-12-12 21:12:08 +0900

    tuning strain gauges and settings reset on seeeduino xiao ble

--- app/boards/arm/seeeduino_xiao_ble/board.cmake
+++ app/boards/arm/seeeduino_xiao_ble/board.cmake
@@ -1,7 +1,9 @@
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: MIT
 
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52" "--frequency=4000000")
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
